67a338e5ac17441c7a18b1860682cbe1764664c9
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2022-01-05 11:16:01 +0100

    fix: CasesElement component

--- src/components/CasesElement.vue
+++ src/components/CasesElement.vue
@@ -2,14 +2,14 @@
   <div class="flex justify-center">
     <div id="block" class="w-1/3 py-10 m-3 text-base text-center bg-blue-200 rounded shadow-md">
       <h4 class="text-2xl font-bold text-blue-800">Cases</h4>
-      <p><span class="font-bold">New:</span> {{ stats.NewConfirmed }}</p>
-      <p><span class="font-bold">Total:</span> {{ stats.TotalConfirmed }}</p>
+      <p><span class="font-bold">New:</span> {{ divideNumber(stats.NewConfirmed) }}</p>
+      <p><span class="font-bold">Total:</span> {{ divideNumber(stats.TotalConfirmed) }}</p>
     </div>
 
-    <div id="block" class="w-1/3 py-10 m-3 text-base text-center bg-blue-200 rounded shadow-md">
-      <h4 class="text-2xl font-bold text-blue-800">Cases</h4>
-      <p><span class="font-bold">New:</span> {{ stats.NewConfirmed }}</p>
-      <p><span class="font-bold">Total:</span> {{ stats.TotalConfirmed }}</p>
+    <div id="block" class="w-1/3 py-10 m-3 text-base text-center bg-blue-300 rounded shadow-md">
+      <h4 class="text-2xl font-bold text-blue-800">Deaths</h4>
+      <p><span class="font-bold">New:</span> {{ divideNumber(stats.NewDeaths) }}</p>
+      <p><span class="font-bold">Total:</span> {{ divideNumber(stats.TotalDeaths) }}</p>
     </div>
   </div>
 </template>
@@ -18,6 +18,11 @@
 export default {
   name: "DataBoxes",
   props: ["stats"],
+  methods: {
+    divideNumber(number) {
+      return number.toLocaleString();
+    },
+  },
 };
 </script>
 
