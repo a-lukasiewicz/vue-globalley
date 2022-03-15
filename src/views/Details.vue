cb51a8bac8ac07269fddbc362bfb82aa2f3b5102
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2022-03-15 15:17:00 +0100

    idea for development

--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,16 +1,20 @@
 <template >
     <div id="contaier" class="h-screen overflow-y-scroll snap snap-y snap-mandatory scroll-smooth">
         <section class="w-full h-screen bg-red-500 snap-start">
-            <h1>Site 1</h1>
+            <h1>Daily</h1>
+            <div id="graph"></div>
         </section>
         <section class="w-full h-screen bg-blue-500 snap-start">
-            <h1>Site 2</h1>
+            <h1>Weekly</h1>
+            <div id="graph"></div>
         </section>
         <section class="w-full h-screen bg-green-500 snap-start">
-            <h1>Site 3</h1>
+            <h1>Monthly</h1>
+            <div id="graph"></div>
         </section>
         <section class="w-full h-screen bg-yellow-500 snap-start">
-            <h1>Site 4</h1>
+            <h1>Yearly</h1>
+            <div id="graph"></div>
         </section>
     </div>
 </template>
