cd987d11996eecdeef438feadb871825d80a86f4
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2022-03-24 15:53:14 +0100

    add libaries & hero component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <div :class="isDark ? 'dark' : ''">
+  <div :class="isDark ? 'dark' : ''" class="overflow-x-hidden ">
     <div id="mainContainer" class="w-full h-full dark:bg-gray-700 sm:h-screen">
       <Header />
-      <router-link to="/details">Details</router-link>
       <div>
         <router-view />
       </div>
-      <div class="flex justify-center">
+      <Footer />
+      <!-- <div class="flex justify-center">
         <button @click="isDark = !isDark" class="px-12 py-3 m-3 text-gray-900 transition-all ease-in-out transform border border-gray-200 rounded shadow-sm lg:absolute lg:bottom-5 hover:bg-blue-300 hover:scale-110 hover:text-white hover:rounded-full dark:bg-green-500 dark:border-green-500 dark:text-white">Change theme colors</button>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue"
 import CountryDetails from "./components/CountryDetails.vue";
 import CasesElement from "./components/CasesElement.vue";
 
 export default {
   components: {
     Header,
+    Footer,
     CountryDetails,
     CasesElement,
   },
