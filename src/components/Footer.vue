d5759ff4ef60717cd551e056414e3064c124160b
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2022-03-24 15:51:17 +0100

    add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,41 @@
+<template lang="">
+<div class="bg-gray-900 ">
+    <div class="max-w-2xl py-10 mx-auto text-white">
+        <div class="text-center">
+            <h3 class="mb-3 text-3xl"> Download our app </h3>
+            <p> Stay updated. </p>
+            <div class="flex justify-center my-10">
+                <div class="flex items-center w-auto px-4 py-2 mx-2 border rounded-lg">
+                    <img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" class="w-7 md:w-8">
+                    <div class="ml-3 text-left">
+                        <p class='text-xs text-gray-200'>Download on </p>
+                        <p class="text-sm md:text-base"> Play Store </p>
+                    </div>
+                </div>
+                <div class="flex items-center w-auto px-4 py-2 mx-2 border rounded-lg">
+                    <img src="https://cdn-icons-png.flaticon.com/512/888/888841.png" class="w-7 md:w-8">
+                    <div class="ml-3 text-left">
+                        <p class='text-xs text-gray-200'>Download on </p>
+                        <p class="text-sm md:text-base"> Apple Store </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col items-center mt-6 text-sm text-gray-400 md:flex-row md:justify-between">
+            <div class="order-1 md:order-2">
+                <span class="px-2">About us</span>
+                <span class="px-2 border-l">Contact us</span>
+                <span class="px-2 border-l">Privacy Policy</span>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    name: "Footer",
+}
+</script>
+<style lang="">
+    
+</style>
