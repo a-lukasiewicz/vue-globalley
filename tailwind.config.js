2279c9ed0aedf2308489e3eca0e492017cb8f0c4
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2022-03-29 09:41:38 +0200

    fix project config & change home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  purge: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: "class", // or 'media' or 'class'
+  purge: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  mode: 'jit',
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+  },
+  daisyui: {
+    themes: [],
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-};
+  plugins: [require("daisyui")],
+}
