a81d8fd36d4ddcd0cf599cc15ccf3a9876ec29ee
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2022-03-29 10:56:01 +0200

    fixed contact page & vercel build

--- src/components/RandomNews.vue
+++ src/components/RandomNews.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-3/4 p-3 mb-3 shadow-xl card lg:card-side bg-base-100">
   <figure><img src="https://api.lorem.space/image/album?w=400&h=400" alt="Album"></figure>
-  <div class="bg-gray-400 card-body">
+  <div class="card-body">
     <h2 class="card-title">News Title</h2>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis ex qui corporis distinctio fugit ducimus earum libero nihil, ut harum suscipit commodi sapiente facilis omnis! Corrupti ducimus ex facere officiis aliquam dolor perferendis, voluptatum laudantium odio magni incidunt exercitationem tenetur in a cupiditate similique accusamus sed aperiam debitis et mollitia!</p>
     <div class="justify-end card-actions">
--- src/views/Details.vue
+++ src/views/Details.vue
@@ -1,21 +1,7 @@
 <template >
-    <div id="contaier" class="h-screen overflow-y-scroll snap snap-y snap-mandatory scroll-smooth">
-        <section class="w-full h-screen bg-red-500 snap-start">
-            <h1>Daily</h1>
-            <div id="graph"></div>
-        </section>
-        <section class="w-full h-screen bg-blue-500 snap-start">
-            <h1>Weekly</h1>
-            <div id="graph"></div>
-        </section>
-        <section class="w-full h-screen bg-green-500 snap-start">
-            <h1>Monthly</h1>
-            <div id="graph"></div>
-        </section>
-        <section class="w-full h-screen bg-yellow-500 snap-start">
-            <h1>Yearly</h1>
-            <div id="graph"></div>
-        </section>
+    <div id="contaier">
+        <h1>Daily</h1>
+        
     </div>
 </template>
 <script>
